180d7ef6ec24cea3be98d77f83bc4ec9b51f741a
”Tsuruta_Yoshihiro” <ytsuruta720@gmail.com>
2020-03-02 12:44:30 +0000

    fashonista0302

---
+++ fashonista/resources/views/layouts/user.balad.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        
+        <!--CSRF Token-->
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+        
+        {{--各ページごとにtitleタグをいれる@yield--}}
+        <title>@yield('title')</title>
+        
+        <!--Scripts-->
+        
+    </head>
+</html>
--- fashonista/routes/web.php
+++ fashonista/routes/web.php
@@ -17,4 +17,23 @@ Route::get('/', function () {
 
 Route::group(['prefix' => 'user'], function(){
     Route::get('coordination/create', 'User\CoordinationController@add');
+    Route::post('coordination/create', 'User\CoordinationController@create');
+    
+    Route::get('coordination', 'User\CoordinationController@index');
+    
+    Route::get('coordination/edit', 'User\CoordinationController@edit');
+    Route::post('coordination/edit', 'User\CoordinationController@update');
+    
+    Route::get('coordination/delete', 'User\CoordinationController@delete');
+
+    
+    
+    
+    Route::get('profile/create', 'User\ProfileController@add');
+    Route::post('profile/create', 'User\ProfileController@create');
+    
+    Route::get('profile/edit', 'User\ProfileController@edit');
+    Route::post('profile/edit', 'User\ProfileController@update');
 });
+
+
