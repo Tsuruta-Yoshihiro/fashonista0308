497e0615b3f900df960ad246be6ef7163abbaf4f
”Tsuruta_Yoshihiro” <ytsuruta720@gmail.com>
2020-03-08 09:03:43 +0000

    fashonista0308

--- fashonista/resources/views/user/profile/create.blade.php
+++ fashonista/resources/views/user/profile/create.blade.php
@@ -1,13 +1,16 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        
-        <title>Myprofile</title>
-    </head>
-    <body>
-        <h1>Myプロフィール作成画面</h1>
-    </body>
-</html>
+{{-- layouts/profile.blade.phpを読み込む --}}
+@extends('layouts.profile')
+
+{{-- profile.blade.phpの@yield('title')に'新規登録ページ'を埋め込む --}}
+@section('title', '新規登録ページ')
+
+{{-- profile.blade.phpの@yield('content')に以下タグを埋め込む --}}
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 mx-auto">
+                <h2>新規登録ページ</h2>
+            </div>
+        </div>
+    </div>
+@endsection
